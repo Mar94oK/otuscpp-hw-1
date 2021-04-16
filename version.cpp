64583a719eeccfdcf290b950f2c18a73a6e20c10
Maxim Uvarenkov <maxim.uvarenkov@gs-labs.ru>
2021-04-16 21:16:53 +0300

    Adding Versioning Functionality

--- version.cpp
+++ version.cpp
@@ -3,9 +3,8 @@
 namespace MyHomework
 {
 	const int HomeWorkMajorNumber = 1;
-	const int HomeWorkMinorNumber = 0;
-	const int HomeWorkBuildNumber = 0;
+	const int HomeWorkMinorNumber = 3;
+	const int HomeWorkBuildNumber = 9;
 
-	const char *BuildTime = "2021-04-08T18:02:34";
+	const char *BuildTime = "2021-04-16T21:15:23";
 }
-
